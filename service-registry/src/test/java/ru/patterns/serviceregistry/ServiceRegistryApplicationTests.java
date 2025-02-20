e33f9d98f1c796794f55b3f85de45e1b8a4a7bcb
Aleksandr Kirsanov <alex.kirsanov182@gmail.com>
2025-02-20 11:38:26 +0700

    Добавлен service-registry с Eureka

---
+++ service-registry/src/test/java/ru/patterns/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.patterns.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
