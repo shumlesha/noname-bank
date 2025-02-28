f4c08fdda4bcfcad1e8b4fa2704bfd7e74287ad3
Alexey Shumkov <shumleshas@gmail.com>
2025-02-28 22:53:56 +0700

    Добавлен базовый employee-интерфейс

---
+++ employee-interface/src/main/java/com/bank/employeeinterface/ViewController.java
@@ -0,0 +1,30 @@
+package com.bank.employeeinterface;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+public class ViewController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register";
+    }
+
+    @GetMapping("/home")
+    public String homePage() {
+        return "home";
+    }
+
+    @GetMapping("/")
+    public String indexPage() {
+        return "redirect:/employee/home";
+    }
+}
