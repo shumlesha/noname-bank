519bcf1c0a2fe80c57be7037e00d7d3cbb34e1c8
Alexey Shumkov <shumleshas@gmail.com>
2025-03-04 23:08:16 +0700

    Исправлен парсинг хидера авторизации при проверке токена

--- auth-service/src/main/java/com/bank/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/bank/authservice/controller/AuthController.java
@@ -13,10 +13,12 @@ import com.bank.authservice.util.ResponseBuilder;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -77,7 +79,7 @@ public class AuthController {
     @PostMapping("/verify")
     @Operation(summary = "Verify access token")
     public ResponseEntity<DefaultResponse<TokenVerificationDto>> verifyAccessToken(
-            @Header("Authorization") String authHeader) {
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader) {
         TokenVerificationDto result = authService.verifyAccessToken(authHeader);
         return ResponseEntity.ok(
                 ResponseBuilder.success(
