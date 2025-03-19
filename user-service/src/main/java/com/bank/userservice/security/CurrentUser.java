bc5040bb0965ffa15d5d6b29450f9a0a8effebfa
Alexey Shumkov <shumleshas@gmail.com>
2025-03-19 19:08:25 +0700

    В user-service добавлена поддержка Keycloak

--- user-service/src/main/java/com/bank/userservice/security/CurrentUser.java
+++ user-service/src/main/java/com/bank/userservice/security/CurrentUser.java
@@ -3,26 +3,34 @@ package com.bank.userservice.security;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
+import java.security.Principal;
 import java.util.Collection;
+import java.util.Map;
 import java.util.UUID;
 
 @Data
 @RequiredArgsConstructor
-public class CurrentUser implements UserDetails {
+public class CurrentUser implements Principal, OAuth2AuthenticatedPrincipal {
     private final UUID id;
     private final String email;
-    private final String fullName;
-    private final Collection<? extends GrantedAuthority> authorities;
-
-
-    @Override
-    public String getPassword() {
-        return "";
-    }
+    private final Map<String, Object> attributes;
+    private final Collection<GrantedAuthority> authorities;
 
     @Override
-    public String getUsername() {
+    public String getName() {
         return email;
     }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+
 }
--- user-service/src/main/java/com/bank/userservice/security/config/SecurityConfig.java
+++ user-service/src/main/java/com/bank/userservice/security/config/SecurityConfig.java
@@ -1,29 +1,46 @@
 package com.bank.userservice.security.config;
 
-import com.bank.userservice.security.JwtFilter;
-import com.bank.userservice.util.JwtUtil;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
+@EnableMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true
+)
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final JwtUtil jwtUtil;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final CustomAccessDeniedHandler customAccessDeniedHandler;
+
+    private final KeycloakJwtAuthenticationConverter keycloakJwtAuthenticationConverter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -36,17 +53,14 @@ public class SecurityConfig {
                                         "/swagger-ui/**",
                                         "/v3/api-docs/**").permitAll()
                                 .anyRequest().authenticated())
-                .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class)
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(keycloakJwtAuthenticationConverter)))
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(jwtAuthenticationEntryPoint)
                         .accessDeniedHandler(customAccessDeniedHandler));
 
         return http.build();
     }
 
-    @Bean
-    JwtFilter jwtFilter() {
-        return new JwtFilter(jwtUtil);
-    }
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
