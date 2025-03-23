cc6a56273e8a985c83c7f1d0aaa740aa6d88d4e5
Alexey Shumkov <shumleshas@gmail.com>
2025-03-24 02:40:50 +0700

    Изменен Gateway config

--- gateway/src/main/java/ru/patterns/gateway/configuration/gateway/GatewayConfiguration.java
+++ gateway/src/main/java/ru/patterns/gateway/configuration/gateway/GatewayConfiguration.java
@@ -14,8 +14,6 @@ public class GatewayConfiguration {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("auth-service", r -> r.path("/api/auth/**")
-                        .uri("lb://auth-service"))
                 .route("user-service", r -> r.path("/api/users/**")
                         .filters(f -> f
                                 .filter(customTokenRelayFactory.apply())
@@ -40,17 +38,12 @@ public class GatewayConfiguration {
                         .filters(f -> f.setPath("/client/home"))
                         .uri("lb://client-interface"))
                 .route("client-interface", r -> r.path("/client/**")
-                        //.filters(f -> f.filter(customTokenRelayFactory.apply()))
                         .uri("lb://client-interface"))
                 .route("employee-root-redirect", r -> r.path("/employee", "/employee/")
                         .filters(f -> f.redirect(302, "/employee/me"))
                         .uri("lb://employee-interface"))
                 .route("employee-interface", r -> r.path("/employee/**")
-                        //.filters(f -> f.filter(customTokenRelayFactory.apply()))
                         .uri("lb://employee-interface"))
-//                .route("root-redirect", r -> r.path("/")
-//                        .filters(f -> f.redirect(302, "/client/login"))
-//                        .uri("lb://client-interface"))
                 .build();
     }
 }
