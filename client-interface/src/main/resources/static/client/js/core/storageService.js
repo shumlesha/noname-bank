9886d667dc5950b5f3aaa3bbf7ed2a3876cf3ef5
Aleksandr Kirsanov <alex.kirsanov182@gmail.com>
2025-03-29 00:22:28 +0700

    Небольшое изменение в UI

--- client-interface/src/main/resources/static/client/js/core/storageService.js
+++ client-interface/src/main/resources/static/client/js/core/storageService.js
@@ -1,28 +1,6 @@
 import {config} from '../config/config.js';
 
 class StorageService {
-
-    saveTokens(tokenData) {
-        localStorage.setItem(config.storage.tokens, JSON.stringify(tokenData));
-    }
-
-    getTokens() {
-        const tokensStr = localStorage.getItem(config.storage.tokens);
-        if (!tokensStr) return null;
-
-        try {
-            return JSON.parse(tokensStr);
-        } catch (error) {
-            console.error("Ошибка разбора токенов:", error);
-            localStorage.removeItem(config.storage.tokens);
-            return null;
-        }
-    }
-
-    removeTokens() {
-        localStorage.removeItem(config.storage.tokens);
-    }
-
     saveUserData(userData) {
         localStorage.setItem(config.storage.userData, JSON.stringify(userData));
     }
