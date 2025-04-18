8e915464da3bc606cb48a0648831315be6147c64
Alexey Shumkov <shumleshas@gmail.com>
2025-04-18 18:18:12 +0700

    Исправление заголовка

--- notification-service/src/main/java/com/bank/notificationservice/service/idempotency/IdempotencyRecordInterceptor.java
+++ notification-service/src/main/java/com/bank/notificationservice/service/idempotency/IdempotencyRecordInterceptor.java
@@ -24,7 +24,7 @@ public class IdempotencyRecordInterceptor implements RecordInterceptor<String, T
     public ConsumerRecord<String, TransactionEntry> intercept(ConsumerRecord<String, TransactionEntry> record,
                                                               Consumer<String, TransactionEntry> consumer) {
         String msgId = Optional.ofNullable(
-                        record.headers().lastHeader("messageId"))
+                        record.headers().lastHeader("X-idempotency-key"))
                 .map(header -> new String(header.value(), StandardCharsets.UTF_8))
                 .orElse(record.topic() + "-" + record.partition() + "-" + record.offset());
 
