b1e6bf8e01cd7903a1a476c8c7b76f4627a1d131
Aleksandr Kirsanov <alex.kirsanov182@gmail.com>
2025-04-19 00:08:43 +0700

    Добавлен аспект для идемпотентности

---
+++ credit/src/main/java/ru/patterns/credit/configuration/idempotent/IdempotencyAspect.java
@@ -0,0 +1,44 @@
+package ru.patterns.credit.configuration.idempotent;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.server.ResponseStatusException;
+import ru.patterns.credit.infrastructure.idempotency.IdempotencyService;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class IdempotencyAspect {
+    private final IdempotencyService idempotencyService;
+
+    @Around("@annotation(idempotent)")
+    public Object around(ProceedingJoinPoint joinPoint, Idempotent idempotent) throws Throwable {
+        ServletRequestAttributes attrs = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+        HttpServletRequest request = attrs.getRequest();
+
+        String key = switch (idempotent.source()) {
+            case HEADER -> request.getHeader(IdempotencyService.IDEMPOTENCY_KEY_HEADER);
+            case PARAM -> request.getParameter(IdempotencyService.IDEMPOTENCY_KEY_PARAM);
+        };
+
+        if (key == null || key.isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Missing X-idempotency-key");
+        }
+
+        return idempotencyService.execute(key, () -> {
+            try {
+                return joinPoint.proceed();
+            } catch (Throwable throwable) {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error processing request", throwable);
+            }
+        });
+    }
+}
